59733d0e4cda59b08686e4068bbb91894ff970b3
ashes303 <ash96@pitt.edu>
2021-07-27 22:29:06 -0500

    decode JSON response

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, pre)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Http
+import Json.Decode exposing (Decoder, field, string)
 
 
 
@@ -30,12 +33,7 @@ type Model
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading
-  , Http.get
-      { url = "https://elm-lang.org/assets/public-opinion.txt"
-      , expect = Http.expectString GotText
-      }
-  )
+  ( Loading, getRandomCatGif)
 
 
 
@@ -43,20 +41,23 @@ init _ =
 
 
 type Msg
-  = GotText (Result Http.Error String)
+  = MorePlease
+  | GotGif (Result Http.Error String)
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    GotText result ->
+    MorePlease ->
+      (Loading, getRandomCatGif)
+
+    GotGif result ->
       case result of
-        Ok fullText ->
-          (Success fullText, Cmd.none)
+        Ok url ->
+          (Success url, Cmd.none)
 
         Err _ ->
           (Failure, Cmd.none)
-
 
 
 -- SUBSCRIPTIONS
@@ -73,12 +74,40 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
+    div []
+      [ h2 [] [ text "Random Cats" ]
+      , viewGif model
+      ]
+
+
+viewGif: Model -> Html Msg
+viewGif model =
   case model of
     Failure ->
-      text "I was unable to load your book."
+      div []
+        [ text "I could not load a cat gif."
+        , button [ onClick MorePlease] [ text "Try Again"]
+        ]
 
     Loading ->
       text "Loading..."
 
-    Success fullText ->
-      pre [] [ text fullText ]
+    Success url ->
+      div []
+        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
+        , img [ src url] []
+        ]
+
+-- Functions
+
+getRandomCatGif: Cmd Msg
+getRandomCatGif =
+  Http.get 
+    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+    , expect = Http.expectJson GotGif gifDecoder
+    }
+
+gifDecoder: Decoder String
+gifDecoder =
+  field "data" (field "image_url" string)
+
