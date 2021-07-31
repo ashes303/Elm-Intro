5bec080a403824d1ac104b52471f769e67d42f96
ashes303 <ash96@pitt.edu>
2021-07-31 12:59:20 -0500

    Random

--- src/Main.elm
+++ src/Main.elm
@@ -2,112 +2,61 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Http
-import Json.Decode exposing (Decoder, field, string)
+import Random
 
+-- Main
 
-
--- MAIN
-
-
-main =
+main = 
   Browser.element
-    { init = init
-    , update = update
-    , subscriptions = subscriptions
-    , view = view
-    }
+  { init = init
+  , update = update
+  , subscriptions = subscriptions
+  , view = view
+  }
 
+-- Model
 
-
--- MODEL
-
-
-type Model
-  = Failure
-  | Loading
-  | Success String
-
+type alias Model =
+  { dieFace : Int
+  }
 
 init : () -> (Model, Cmd Msg)
 init _ =
-  ( Loading, getRandomCatGif)
+  ( Model 1 
+  , Cmd.none
+  )
 
-
-
--- UPDATE
-
+-- Update
 
 type Msg
-  = MorePlease
-  | GotGif (Result Http.Error String)
-
+ = Roll
+ | NewFace Int 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    MorePlease ->
-      (Loading, getRandomCatGif)
+    Roll ->
+      ( model 
+      , Random.generate NewFace (Random.int 1 6)
+      )
 
-    GotGif result ->
-      case result of
-        Ok url ->
-          (Success url, Cmd.none)
+    NewFace newFace ->
+      ( Model newFace
+      , Cmd.none
+      )
 
-        Err _ ->
-          (Failure, Cmd.none)
-
-
--- SUBSCRIPTIONS
-
+-- Subscriptions
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
-
-
--- VIEW
-
+-- View
 
 view : Model -> Html Msg
 view model =
-    div []
-      [ h2 [] [ text "Random Cats" ]
-      , viewGif model
-      ]
-
-
-viewGif: Model -> Html Msg
-viewGif model =
-  case model of
-    Failure ->
-      div []
-        [ text "I could not load a cat gif."
-        , button [ onClick MorePlease] [ text "Try Again"]
-        ]
-
-    Loading ->
-      text "Loading..."
-
-    Success url ->
-      div []
-        [ button [ onClick MorePlease, style "display" "block" ] [ text "More Please!" ]
-        , img [ src url] []
-        ]
-
--- Functions
-
-getRandomCatGif: Cmd Msg
-getRandomCatGif =
-  Http.get 
-    { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
-    , expect = Http.expectJson GotGif gifDecoder
-    }
-
-gifDecoder: Decoder String
-gifDecoder =
-  field "data" (field "image_url" string)
-
+  div []
+    [ h1 [] [ text (String.fromInt model.dieFace)]
+    , button [ onClick Roll ] [ text "Roll" ]
+    ]
