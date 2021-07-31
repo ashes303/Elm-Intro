dd82ed0b636121651b42282570b0208a8e459b91
ashes303 <ash96@pitt.edu>
2021-07-31 16:51:16 -0500

    Word finder game

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,19 @@
-port module Main exposing (..)
+module Main exposing (..)
 
 import Browser
+import Http
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode as D
+import Char
 
 
 
 -- MAIN
 
 
-main : Program () Model Msg
+main : Program InitFlags Model Msg
 main =
   Browser.element
     { init = init
@@ -21,28 +23,22 @@ main =
     }
 
 
-
-
--- PORTS
-
-
-port sendMessage : String -> Cmd msg
-port messageReceiver : (String -> msg) -> Sub msg
-
-
-
 -- MODEL
 
 
 type alias Model =
   { draft : String
-  , messages : List String
+  , words : List String
+  , letters : List Char
   }
 
+type alias InitFlags =
+  { letters : String
+  }
 
-init : () -> ( Model, Cmd Msg )
+init : InitFlags -> ( Model, Cmd Msg )
 init flags =
-  ( { draft = "", messages = [] }
+  ( { draft = "", words = [] , letters = String.toList flags.letters }
   , Cmd.none
   )
 
@@ -53,14 +49,9 @@ init flags =
 
 type Msg
   = DraftChanged String
-  | Send
-  | Recv String
+  | Submit
+  | WordChecked (Result Http.Error String)
 
-
--- Use the `sendMessage` port when someone presses ENTER or clicks
--- the "Send" button. Check out index.html to see the corresponding
--- JS where this is piped into a WebSocket.
---
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
@@ -69,28 +60,29 @@ update msg model =
       , Cmd.none
       )
 
-    Send ->
+    Submit ->
       ( { model | draft = "" }
-      , sendMessage model.draft
+      , validateWord model.draft model.letters
       )
 
-    Recv message ->
-      ( { model | messages = model.messages ++ [message] }
-      , Cmd.none
-      )
+    WordChecked result ->
+      case result of 
+        Ok word ->
+          ( { model | words = model.words ++ [word] }
+          , Cmd.none
+          )
+
+        Err error ->
+          Debug.log (Debug.toString error)
+          ( model, Cmd.none)
 
 
 
 -- SUBSCRIPTIONS
 
-
--- Subscribe to the `messageReceiver` port to hear about messages coming in
--- from JS. Check out the index.html file to see how this is hooked up to a
--- WebSocket.
---
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  messageReceiver Recv
+  Sub.none
 
 
 
@@ -100,26 +92,66 @@ subscriptions _ =
 view : Model -> Html Msg
 view model =
   div []
-    [ h1 [] [ text "Echo Chat" ]
+    [ h1 [] [ text "Word Finder" ]
+    , if List.length model.words > 0 then h2 [] [ text "Found Words" ] else text ""
     , ul []
-        (List.map (\msg -> li [] [ text msg ]) model.messages)
+        (List.map (\word -> li [] [ text word ]) model.words)
+    , h2 [] [ text "Letters" ]
+    , ul []
+        (List.map (\letter -> li [] [ text (String.fromChar letter) ]) model.letters)
     , input
         [ type_ "text"
-        , placeholder "Draft"
         , onInput DraftChanged
-        , on "keydown" (ifIsEnter Send)
+        , on "keydown" (ifIsEnter Submit)
         , value model.draft
         ]
         []
-    , button [ onClick Send ] [ text "Send" ]
+    , button [ onClick Submit ] [ text "Submit" ]
     ]
 
 
 
 -- DETECT ENTER
 
-
 ifIsEnter : msg -> D.Decoder msg
 ifIsEnter msg =
   D.field "key" D.string
     |> D.andThen (\key -> if key == "Enter" then D.succeed msg else D.fail "some other key")
+
+-- VALIDATE WORD
+
+validateWord : String -> List Char -> Cmd Msg
+validateWord word letters =
+  if isWordLongEnough word && isWordMadeOfValidLetters word letters then 
+    getWordFromDictonary word
+  else
+    Cmd.none
+
+isWordLongEnough : String -> Bool
+isWordLongEnough word =
+  String.length word >= 3
+
+
+isWordMadeOfValidLetters : String -> List Char -> Bool
+isWordMadeOfValidLetters word letters =
+  String.all (\letter -> List.member letter letters) word
+
+
+
+-- CHECK DICTIONARY
+
+getWordFromDictonary : String -> Cmd Msg
+getWordFromDictonary word =
+  Http.get 
+    { url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/" ++ word 
+    , expect = Http.expectJson WordChecked wordDecoder
+    }
+
+wordDecoder : D.Decoder String
+wordDecoder =
+  D.index 0 (D.field "word" D.string)
+
+
+
+
+
